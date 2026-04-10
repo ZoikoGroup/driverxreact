8a0669417de82520d88248efa6664d9be3bfe004
sumonklyn <sumonklyn@gmail.com>
2026-04-10 16:51:22 +0530

    headerfooter

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import { isLoggedIn } from "../utils/auth";
 
 export default function ForgotPasswordPage() {
