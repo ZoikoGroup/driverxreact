dcac8fe691abae4845691c2f8c4f38ac3be3d566
sumonklyn <sumonklyn@gmail.com>
2026-04-22 18:58:30 +0530

    request a demo

--- app/request-a-demo/DemoSection.tsx
+++ app/request-a-demo/DemoSection.tsx
@@ -52,7 +52,7 @@ export default function DemoSection() {
     setErrorMsg("");
 
     try {
-      const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL+"/api/form/request-a-demo", {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL+"/api/form/request-a-demo/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
