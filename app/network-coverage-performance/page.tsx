e2c4ef3a6cf532f64a5e972d845f267c7b0f748f
Anowar Sadat <anowar.vista@gmail.com>
2026-05-12 15:36:38 +0530

    meta2

--- app/network-coverage-performance/page.tsx
+++ app/network-coverage-performance/page.tsx
@@ -1,10 +1,15 @@
 import NetworkFeatures from "./ourcompanies";
 
-export default function plans(){
+export const metadata = {
+  title: "Network Coverage & Performance | DriverX Mobile",
+  description:
+    "Stay connected coast to coast with DriverX Mobile’s strong network and high-speed performance, ensuring your fleet runs efficiently wherever the road leads.",
+};
 
-    return(
-        <>
-        <NetworkFeatures/>
-        </>
-    );
+export default function NetworkPage() {
+  return (
+    <>
+      <NetworkFeatures />
+    </>
+  );
 }
