bdfe2241a15a5dd6f5744ebe2a92e94341349ad3
Anowar Sadat <anowar.vista@gmail.com>
2026-05-13 13:52:02 +0530

    meta5

--- app/gig-delivery-driver-plans/page.tsx
+++ app/gig-delivery-driver-plans/page.tsx
@@ -1,11 +1,15 @@
 import Gig from "./GIg";
 
+export const metadata = {
+  title: "Gig & Delivery Driver Phone Plans | DriverX Mobile",
+  description:
+    "DriverX offers the best value for gig and delivery driver phone plans. Enjoy 5G speeds, hotspot & rewards. Plans start at just $29/month—no contract required.",
+};
 
-export default function PlansPage() {
+export default function GigPlansPage() {
   return (
-
     <>
-    <Gig/>
+      <Gig />
     </>
   );
 }
--- app/top-up-plans/page.tsx
+++ app/top-up-plans/page.tsx
@@ -1,12 +1,21 @@
 import Topupplans from "./Topupplans";
 
+export const metadata = {
+  title: "Best Mobile Phone Top-Up Plans for Drivers | DriverX Mobile",
+  description:
+    "Need more data? DriverX Top-Up Plans offer 3GB or 6GB of high-speed data starting at $8.99. Stackable, 30-day add-ons perfect for gig drivers on the go.",
+  keywords: [
+    "mobile phone top up plans",
+    "DriverX top up plans",
+    "data add-on plans",
+    "mobile recharge add-ons",
+  ],
+};
 
-export default function Plan(){
-
-
-    return(
-        <div className="dark:bg-gray-950">
-<Topupplans/>
-        </div>
-    );
+export default function TopUpPlansPage() {
+  return (
+    <div className="dark:bg-gray-950">
+      <Topupplans />
+    </div>
+  );
 }
