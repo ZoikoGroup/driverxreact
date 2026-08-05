23096535d90b43ec447583dc657cba820c54e21b
sumonklyn <sumonklyn@gmail.com>
2026-08-05 12:51:33 +0530

    Remove auth session API route

--- app/api/auth/session/route.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth";
-
-const handler = NextAuth({
-  providers: [], // your providers here e.g. GoogleProvider, CredentialsProvider
-  secret: process.env.NEXTAUTH_SECRET,
-});
-
-export { handler as GET, handler as POST };
